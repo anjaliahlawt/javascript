22f5098668c71dd742285a91a154d9afbb05b486
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-06-05 10:07:35 +0530

    inital commit

--- 1.js
+++ 1.js
@@ -3,3 +3,16 @@
 let firstname="anjali";
 let lastname="ahlawat";
 console.log("good morning"+" "+firstname+" "+lastname);
+
+//write the program to greet a person given their gender.(if-else)
+
+let iswomen=false;
+
+  if(iswomen==true)
+    {
+        console.log("hello mam");
+    }
+    else
+    {
+        console.log("hello sir");
+    }
