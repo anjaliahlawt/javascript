3907573f23e39b69c709cfa0da1f49cc77b2b24c
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-06-05 11:10:30 +0530

    inital commit

--- 1.js
+++ 1.js
@@ -1,18 +1,57 @@
 // write the program to greet a person given their first and last name.
 
-let firstname="anjali";
-let lastname="ahlawat";
-console.log("good morning"+" "+firstname+" "+lastname);
+// let firstname="anjali";
+// let lastname="ahlawat";
+// console.log("good morning"+" "+firstname+" "+lastname);
 
 //write the program to greet a person given their gender.(if-else)
 
-let iswomen=false;
+// let iswomen=false;
 
-  if(iswomen==true)
+//   if(iswomen==true)
+//     {
+//         console.log("hello mam");
+//     }
+//     else
+//     {
+//         console.log("hello sir");
+//     }
+
+// write a program that countsfrom 0to 100 and prints.
+
+// let a=0;
+// for(a=0;a<=10000;a++)
+//     {
+//         console.log(a);
+//     }
+
+ //write a program prints all the even numbers in an array
+ 
+//  const arr=[20,22,24,26,28];
+//  for(let i=0;i<arr.length;i++)
+//     {
+//         if(arr[i]%2==0)
+//             {
+//                 console.log("is even number"+" "+arr[i])
+//             }
+//             else
+//             {
+//                 console.log("is not a even number"+arr[i]);
+//             }
+//     }
+    
+// write a program to print the biggest number in an array
+
+let arr=[20,23,25,12];
+let max=arr[0];
+for(let i=1;i<=arr.length;i++)
+
     {
-        console.log("hello mam");
+        if(arr[i]>max)
+            {   
+                 max=arr[i];
+               
+            }
+            
     }
-    else
-    {
-        console.log("hello sir");
-    }
+    console.log(max+" "+"is a biggest number in array");
