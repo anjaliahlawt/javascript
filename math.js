6e49ba8753271644dcd6c8d295ab68957be7d44d
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-06-07 12:46:58 +0530

    inital commit

---
+++ math.js
@@ -0,0 +1,79 @@
+//1. Math.round()
+
+//The Math.round() function rounds a number to the nearest integer.
+
+
+let num1 = 4.7;
+let roundedNum = Math.round(num1);
+console.log(roundedNum);  
+
+
+//2. Math.floor()
+
+//The Math.floor() function rounds a number down to the nearest integer.
+
+
+let num2 = 4.7;
+let flooredNum = Math.floor(num2);
+console.log(flooredNum);  
+
+//3. Math.ceil()
+
+//The Math.ceil() function rounds a number up to the nearest integer.
+
+let num = 4.2;
+let ceiledNum = Math.ceil(num);
+console.log(ceiledNum);  
+
+//4. Math.max()
+
+//The Math.max() function returns the largest of zero or more numbers.
+
+let maxNum = Math.max(10, 20, 30);
+console.log(maxNum);  
+
+//5. Math.min()
+
+//The Math.min() function returns the smallest of zero or more numbers.
+
+let minNum = Math.min(10, 20, 30);
+console.log(minNum);  
+
+//6. Math.random()
+
+//The Math.random() function returns a random number between 0 (inclusive) and 1 (exclusive).
+
+let randomNum = Math.random();
+console.log(randomNum);  
+
+//7. Math.sqrt()
+
+//The Math.sqrt() function returns the square root of a number
+
+let num3 = 16;
+let sqrtNum = Math.sqrt(num3);
+console.log(sqrtNum);  
+
+// Math.pow()
+
+//The Math.pow() function returns the base to the exponent power, that is, base^exponent.
+
+let base = 2;
+let exponent = 3;
+let power = Math.pow(base, exponent);
+console.log(power);  
+
+//9. Math.abs()
+
+//The Math.abs() function returns the absolute value of a number.
+
+let num4= -10;
+let absoluteValue = Math.abs(num4);
+console.log(absoluteValue);  
+
+//10. Math.PI
+
+//The Math.PI property represents the ratio of the circumference of a circle to its diameter, approximately 3.14159.
+
+let piValue = Math.PI;
+console.log(piValue);  
