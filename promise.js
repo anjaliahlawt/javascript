344b93b1f8c7e18dee62e84c614d834d5a16acd9
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-06-14 17:33:08 +0530

    inital commit

--- promise.js
+++ promise.js
@@ -62,3 +62,68 @@ const myPromise=new Promise((resolve,reject)=>
             console.log("asy opertion complted");
         });  
  
+ //chaining promise
+
+
+        function firstOperation() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => resolve("First operation complete"), 1000);
+            });
+        }
+        
+        function secondOperation(message) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => resolve(`${message} -> Second operation complete`), 1000);
+            });
+        }
+        
+        function thirdOperation(message) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => resolve(`${message} -> Third operation complete`), 1000);
+            });
+        }
+        
+        firstOperation()
+            .then((result) => {
+                console.log(result);
+                return secondOperation(result);
+            })
+            .then((result) => {
+                console.log(result);
+                return thirdOperation(result);
+            })
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+//async keyword used 
+
+async function data()
+{
+    return 'data fetched';
+}
+data()
+.then((result)=>
+    {
+        console.log(result);
+    });
+
+
+ //The await Keyword
+//The await keyword is used inside async functions to pause the execution of the function until the Promise is resolved or rejected. It can only be used within an async function.   
+
+async function data1()
+{
+  let promise=new Promise((resolve,reject)=>
+    {
+        setTimeout(()=>
+            {
+                console.log(" information fetched");
+            },2000);
+    });
+    let result=await promise;
+    console.log(result);
+}
+data1();
